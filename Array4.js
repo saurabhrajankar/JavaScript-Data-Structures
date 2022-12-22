5e563dfa4e9b64710bc765c534fe6f79899d364c
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-12-22 09:34:47 +0530

    Program To Find Sum Of 3 Numbers Whose Sum Is 0

---
+++ Array4.js
@@ -0,0 +1,19 @@
+let arr = [0, -1, 2, -3, 1];
+
+for(i=0;i<arr.length;i++)
+{
+    for(j=i+1;j<arr.length;j++)
+    {
+        for(k=j+1;j<arr.length;k++)
+        {
+            if(arr[i]+arr[j]+arr[k]==0)
+            {
+                console.log("Numbers Are:")
+                console.log(arr[i])
+                console.log(arr[j])
+                console.log(arr[j])
+                break;
+            }
+        }
+    }
+}
